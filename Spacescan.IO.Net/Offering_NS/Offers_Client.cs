9cc121032a61ff7139435c0f082099d5c0863494
Julian B <bechti44@googlemail.com>
2023-02-10 20:34:00 +0100

    initialisation

---
+++ Spacescan.IO.Net/Offering_NS/Offers_Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacescan.IO.Net.Offering_NS
+{
+    internal class Offers_Client
+    {
+    }
+}
